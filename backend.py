281cfb1207ea6f6ce7c2a97ef71e8cac9e2ef918
ameaaira <53601175+ameaaira@users.noreply.github.com>
2019-11-18 11:56:56 +0800

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 # PLAN:
 #   1. USER INPUTS DATA  (TREAT AS AN OBJECT - MAKE DICTIONARY)
 #          - CHOICE: INCOME OR EXPENSE
+#          - ENTER DATE
 #          - CHOOSE EXPENSE TYPE
 #          - DISPLAYS CALCULATOR () TO ENTER THE AMOUNT
 #          - CHOICE IF HE'LL ENTER NOTE
@@ -10,7 +11,24 @@
 #   3. ACCESS DATA FROM THE FILE
 #           - READ THE FILE TO SEE THE DATA
 #   4. DISPLAY THE LIST OF INCOME AND EXPENSES AND THE OTHER DATA PER DAY
-#   5. MAKE AN ALGORITHM THAT WILL COMPUTE FOR THE REMAINING BALANCE EVERYTIME THE USER INPUTS INCOME OR EXPENSE (PER DAY/ PER MONTH) [aira]
+#   5. MAKE AN ALGORITHM THAT WILL COMPUTE FOR THE REMAINING BALANCE EVERYTIME THE USER INPUTS INCOME OR EXPENSE (PER DAY/ PER MONTH)
 #
 #
-#
+# NOTE: get one input ng isang object tas ilagay sa dictionary and put sa file
+# DIFF CLASS: EXPENSE OR INCOME
+
+
+class income():
+    def __init__(date, types, amount, note):
+        self.date = date
+        self.type = types
+        self.amount = amount
+        self.note = note
+
+
+date = raw_input('Enter date: ')
+types = raw_input('Enter types: ')
+amount = raw_input('Enter amount: ')
+note = raw_input('Enter note: ')
+
+new_object = myStruct(name, salary, dob, title)
